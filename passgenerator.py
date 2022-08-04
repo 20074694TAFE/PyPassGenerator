7fea304e3e2496c7f7e5a2bbd6f1d60aa63b5e9e
20074694TAFE <20074694@tafe.wa.edu.au>
2022-08-04 10:32:04 +0800

    more work

--- passgenerator.py
+++ passgenerator.py
@@ -1,7 +1,11 @@
 #This is a comment
 import random
 password = ""
+while True:
+    password_length = input("How long you want password: ")
+    if int(password_length) > 0 and int(password_length) <= 10:
+        break
 for i in range(10):
-    random_integer = random.randint(0,126)
+    random_integer = random.randint(33,126)
     password += chr(random_integer)
 print(password, len(password))
